0401f7de225b98ee3ba5242d57ce40d0ef08004c
Impuls <edurd@rambler.ru>
2024-09-18 09:21:11 +0500

    v1_1

---
+++ app/src/main/java/com/shesound/sapppowertools/DdrgtApp.kt
@@ -0,0 +1,7 @@
+package com.shesound.sapppowertools
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DdrgtApp:Application ()
---
+++ app/src/main/java/com/shesound/sapppowertools/domain/model/Tool.kt
@@ -0,0 +1,8 @@
+package com.shesound.sapppowertools.domain.model
+
+data class Tool(
+    val name: Int,
+    val smallImage: Int,
+    val fullImage: Int,
+    val sound: Int,
+)
