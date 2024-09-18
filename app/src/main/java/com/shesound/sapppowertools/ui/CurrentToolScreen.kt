b5fadd0b3b93c323e952a7f3752b0eab7b501dc0
Impuls <edurd@rambler.ru>
2024-09-18 10:57:24 +0500

    v1_4

--- app/src/main/java/com/shesound/sapppowertools/ui/CurrentToolScreen.kt
+++ app/src/main/java/com/shesound/sapppowertools/ui/CurrentToolScreen.kt
@@ -1,36 +1,43 @@
 package com.shesound.sapppowertools.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.shesound.sapppowertools.R
-import com.shesound.sapppowertools.domain.model.AppState
 import com.shesound.sapppowertools.domain.model.Tool
 import com.shesound.sapppowertools.domain.utils.tools
-import com.shesound.sapppowertools.ui.state.MainEvent
 import com.shesound.sapppowertools.ui.theme.black
+import com.shesound.sapppowertools.ui.theme.red
 import com.shesound.sapppowertools.ui.theme.white
+import com.shesound.sapppowertools.ui.theme.yellow
 
 @Preview(name = "NEXUS_7", device = Devices.NEXUS_7)
 @Preview(name = "NEXUS_7_2013", device = Devices.NEXUS_7_2013)
@@ -49,13 +56,16 @@ import com.shesound.sapppowertools.ui.theme.white
 @Preview(name = "PIXEL_4", device = Devices.PIXEL_4)
 @Preview(name = "PIXEL_4_XL", device = Devices.PIXEL_4_XL)
 @Composable
-fun ToolsScreen(
+fun CurrentToolScreen(
     modifier: Modifier = Modifier,
-    currentTools: List<Tool> = tools,
-    onEvent: (MainEvent) -> Unit = {},
+    tool: Tool = tools[0],
+    onClick: () -> Unit = {},
+    onBackClick: () -> Unit = {}
 ) {
+    val localDensity = LocalDensity.current
+    val cellHeightDp = remember { mutableStateOf(0.dp) }
     BackHandler {
-        onEvent(MainEvent.OnChangeAppState(AppState.Start))
+        onBackClick()
     }
     Box(
         modifier = modifier
@@ -69,38 +79,63 @@ fun ToolsScreen(
             contentDescription = "",
             contentScale = ContentScale.FillBounds
         )
-        Column(
+        Box(
             modifier = modifier
-                .align(alignment = Alignment.TopCenter)
-                .padding(start = 20.dp, end = 20.dp, top = 35.dp, bottom = 50.dp)
+                .align(alignment = Alignment.Center)
                 .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp, top = 35.dp, bottom = 50.dp)
         ) {
+            Image(
+                modifier = modifier
+                    .align(alignment = Alignment.Center)
+                    .fillMaxHeight()
+                    .onGloballyPositioned { coordinates ->
+                        cellHeightDp.value =
+                            with(localDensity) { coordinates.size.height.toDp() }
+                    },
+                painter = painterResource(tool.fullImage),
+                contentDescription = "",
+                contentScale = ContentScale.FillHeight
+            )
             Text(
-                modifier = modifier,
+                modifier = modifier
+                    .align(alignment = Alignment.TopCenter)
+                    .fillMaxWidth()
+                    .padding(top = 15.dp),
                 text = stringResource(R.string.power_tool_sound),
                 style = TextStyle(
-                    fontSize = 25.sp,
+                    fontSize = 22.sp,
                     //fontFamily = FontFamily(Font(R.font.MTS Sans)),
                     fontWeight = FontWeight(600),
                     color = white,
+                    textAlign = TextAlign.Center
                 )
             )
-            LazyColumn(
-                modifier = modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(15.dp)
+            Button(
+                modifier = modifier
+                    .align(alignment = Alignment.BottomCenter)
+                    .padding(bottom = cellHeightDp.value/7),
+                shape = RoundedCornerShape(100.dp),
+                border = BorderStroke(
+                    width = 2.dp,
+                    color = yellow
+                ),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = red
+                ),
+                contentPadding = PaddingValues(20.dp),
+                onClick = onClick
             ) {
-                items(currentTools) { tool ->
-                    ItemTool(
-                        tool = tool,
-                        onClick = {
-                            onEvent(
-                                MainEvent.OnChangeAppState(
-                                    AppState.CurrentTool(tool)
-                                )
-                            )
-                        }
+                Text(
+                    modifier = modifier,
+                    text = stringResource(R.string.start_stop),
+                    style = TextStyle(
+                        fontSize = 25.sp,
+                        //fontFamily = FontFamily(Font(R.font.MTS Sans)),
+                        fontWeight = FontWeight(600),
+                        color = white,
                     )
-                }
+                )
             }
         }
     }
