8508fad9112d9c129c1aeec56f216c708d8e629e
Impuls <edurd@rambler.ru>
2024-09-20 16:15:28 +0500

    v2_2

--- app/src/main/java/com/shesound/sapppowertools/domain/utils/Consts.kt
+++ app/src/main/java/com/shesound/sapppowertools/domain/utils/Consts.kt
@@ -1,5 +1,5 @@
 package com.shesound.sapppowertools.domain.utils
 
 
-const val URL = "https://fumado-vaches.com/"
+const val URL = "https://mucus-gonads.com"
 const val CHECKED_URL = "play.google.com"
